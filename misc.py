1ede058b78f1addccf4c8292bda36bda601dfc5a
muumimorko <teemu.koskinen11@gmail.com>
2021-01-12 16:41:27 +0200

    restructuring 2

--- misc.py
+++ misc.py
@@ -1,41 +1,11 @@
-## misc helper functions
+# misc helper functions
 
 
-# Adds 1 to quadkey value
-def QuadKeyIncrement(qk):
-    level = len(qk)
-    qka = []
-    qka = list(qk)
-    iter = 0
-    while iter < level:
-        qka[iter] = int(qka[iter])
-        iter = iter+1
-    index = level-1
-    carry = 0
-    value = int(qka[index])+1
-    while value > 3:
-        carry = carry+1
-        value = value-4
-    qka[index] = value
-    carrying = False
-    if carry > 0:
-        carrying = True
-    while carrying:
-        if carry > 0:
-            index = index-1
-            qka[index] = qka[index]+carry
-            carry = 0
-            value = qka[index]
-            while value > 3:
-                carry = carry+1
-                value = value-4
-            qka[index] = value
-            if carry == 0:
-                carrying = False
-    return "".join(str(x) for x in qka)
 
 # Find all substrings from a string
 # https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+
+
 def find_all(a_str, sub):
     start = 0
     while True:
@@ -44,3 +14,9 @@ def find_all(a_str, sub):
             return
         yield start
         start += len(sub)  # use start += 1 to find overlapping matches
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
